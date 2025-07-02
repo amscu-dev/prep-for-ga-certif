ca1dda16a6615c85dd3c3e6bb33ebd3474925f0f
amscu-dev <amarinescu.dev@gmail.com>
2025-07-02 23:35:18 +0300

    implement simple action

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-/**
- * The entrypoint for the action. This file simply imports and runs the action's
- * main logic.
- */
-import { run } from './main.js'
+const core = require('@actions/core')
+const github = require('@actions/github')
 
-/* istanbul ignore next */
+async function run() {
+  try {
+    const token = core.getInput('token')
+    const title = core.getInput('title')
+    const body = core.getInput('body')
+    const assignees = core.getInput('assignees')
+
+    const octokit = github.getOctokit(token)
+
+    const response = await octokit.rest.issues.create({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      title,
+      body,
+      assignees: assignees ? assignees.split('\n') : undefined
+    })
+
+    core.setOutput('issue', response.data)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
 run()
